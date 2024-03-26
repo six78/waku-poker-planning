efecc35310b65b46cd449b6e4aa56ca7c1e39acc
aleksey diduh <aleksey.diduh@skyeng.ru>
2024-03-26 12:15:54 -0300

    add home icon

--- src/page-layout/header.component.tsx
+++ src/page-layout/header.component.tsx
@@ -1,7 +1,8 @@
-import { Button, Space } from "antd";
+import { Button, Col, Row, Space } from "antd";
 import useMessage from "antd/es/message/useMessage";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { copyTextToClipboard } from "../shared/clipboard";
+import { HomeOutlined } from "@ant-design/icons";
 
 export function Header() {
   const [messageApi, contextHolder] = useMessage();
@@ -16,12 +17,23 @@ export function Header() {
   return (
     <div className="w-full h-full px-6 bg-white flex items-center justify-end">
       {contextHolder}
-      <Space>
-        <Button onClick={() => copyToClipboard(roomId!)}>Copy room hash</Button>
-        <Button onClick={() => copyToClipboard(location.href)}>
-          Copy game url
-        </Button>
-      </Space>
+      <Row className="w-full" justify="end">
+        <Col span={18}>
+          <Link className="justify-self-start" to="/">
+            <Button type="primary" icon={<HomeOutlined />} />
+          </Link>
+        </Col>
+        <Col span={6}>
+          <Space>
+            <Button onClick={() => copyToClipboard(roomId!)}>
+              Copy room hash
+            </Button>
+            <Button onClick={() => copyToClipboard(location.href)}>
+              Copy game url
+            </Button>
+          </Space>
+        </Col>
+      </Row>
     </div>
   );
 }
